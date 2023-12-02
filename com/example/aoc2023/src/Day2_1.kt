3cfaee91bc485073840a07f868b12a93417a9ab1
Vincent Mercier <vincent.louis.mercier@gmail.com>
2023-12-02 09:17:26 -0500

    On the second day of Xmas, my true love gave to me...

---
+++ com/example/aoc2023/src/Day2_1.kt
@@ -0,0 +1,35 @@
+import java.io.File
+import java.io.InputStream
+import kotlin.streams.asSequence
+
+fun main() {
+    val inputStream: InputStream = File("./com/example/aoc2023/input/day2.txt").inputStream()
+
+    val max = mapOf(
+        "red" to 12,
+        "green" to 13,
+        "blue" to 14
+    )
+
+    val colorRegex = "((?<nbGem>\\d+) (?<color>red|green|blue),? ?)".toRegex()
+    val gameRegex = "Game (?<gameId>\\d+)".toRegex()
+
+    println(
+        inputStream.bufferedReader().lines()
+            .asSequence()
+            .map {
+                val pieces = it.split(":")
+                val gameIdMatch = gameRegex.find(pieces.first())
+                val gameId = gameIdMatch!!.groups["gameId"]!!.value.toInt()
+
+                val gemMatches = colorRegex.findAll(pieces[1])
+
+                return@map gameId to gemMatches
+                    .all {gemMatch ->
+                        max[gemMatch.groups["color"]!!.value]!! >= gemMatch.groups["nbGem"]!!.value.toInt()
+                    }
+            }
+            .filter { it.second }
+            .sumOf { it.first }
+    )
+}
---
+++ com/example/aoc2023/src/Day2_2.kt
@@ -0,0 +1,30 @@
+import java.io.File
+import java.io.InputStream
+import kotlin.streams.asSequence
+
+fun main() {
+    val inputStream: InputStream = File("./com/example/aoc2023/input/day2.txt").inputStream()
+
+    val colorRegex = "((?<nbGem>\\d+) (?<color>red|green|blue),? ?)".toRegex()
+
+    println(
+        inputStream.bufferedReader().lines()
+            .asSequence()
+            .map {
+                val pieces = it.split(":")
+                val gemMatches = colorRegex.findAll(pieces[1])
+
+                gemMatches
+                    .map {gemMatch ->
+                        val color = gemMatch.groups["color"]!!.value
+                        val nbGems = gemMatch.groups["nbGem"]!!.value.toInt()
+
+                        color to nbGems
+                    }
+                    .groupBy { it.first }
+                    .map { it.value.maxBy { it.second }.second }
+                    .reduce { acc, i -> acc * i }
+            }
+            .sum()
+    )
+}
